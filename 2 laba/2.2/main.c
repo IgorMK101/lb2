b890cf149a61029db7217f942066a323fa801c42
IgorMK101 <81213327+IgorMK101@users.noreply.github.com>
2021-08-27 05:25:26 +0300

    Update main.c

--- 2 laba/2.2/main.c
+++ 2 laba/2.2/main.c
@@ -4,7 +4,7 @@
 int main (){
 const int a = 5; const int b =8; const int c = -1;
 float y;
-y = (float) ((a-b)/((2*b) + c))+(1/a);
+y =  ((a-b)/((2*b) + c))+(1/a);
 printf("\n\ty = %.3f\n",y);
     return 0;
 }
